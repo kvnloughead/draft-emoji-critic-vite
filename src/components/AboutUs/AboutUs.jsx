c45e6f290488708cd0316e583b34e4fe6ecd4844
Kevin Loughead <kvnloughead@gmail.com>
2024-01-23 16:51:30 -0500

    nested-routes-after-task

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,9 +1,20 @@
+import { Outlet, Link } from "react-router-dom";
+
 import "./AboutUs.css";
 
 function AboutUs() {
   return (
     <div className="about-us">
+      <ul>
+        <li>
+          <Link to="history">Site History</Link>
+        </li>
+        <li>
+          <Link to="mission">Site Mission</Link>
+        </li>
+      </ul>
       <p>You can find out more information about our site here.</p>
+      <Outlet />
     </div>
   );
 }
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,10 +4,12 @@ import { Routes, Route } from "react-router-dom";
 import Header from "../Header/Header";
 import Dashboard from "../Dashboard/Dashboard";
 import Reviews from "../Reviews/Reviews";
-import AboutUs from "../AboutUs/AboutUs";
-import AboutMe from "../AboutMe/AboutMe";
 
-// New imports
+import AboutUs from "../AboutUs/AboutUs";
+import SiteHistory from "../AboutUs/SiteHistory";
+import SiteMission from "../AboutUs/SiteMission";
+
+import AboutMe from "../AboutMe/AboutMe";
 import MyStory from "../AboutMe/MyStory";
 import Contact from "../AboutMe/Contact";
 import Hobbies from "../AboutMe/Hobbies";
@@ -25,7 +27,10 @@ function App() {
           <Route path="hobbies" element={<Hobbies />} />
           <Route path="my-story" element={<MyStory />} />
         </Route>
-        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/about-us" element={<AboutUs />}>
+          <Route path="mission" element={<SiteMission />} />
+          <Route path="history" element={<SiteHistory />} />
+        </Route>
       </Routes>
     </div>
   );
