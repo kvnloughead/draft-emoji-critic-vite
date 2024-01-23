8e9b3f5cf86b0417d3af41c7b8e13e67e28cc55a
Kevin Loughead <kvnloughead@gmail.com>
2024-01-23 13:22:00 -0500

    adding-a-route-after-task

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,16 +2,18 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "../Dashboard/Dashboard";
+import Reviews from "../Reviews/Reviews";
+import AboutMe from "../AboutMe/AboutMe";
 import Header from "../Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* Wrap a Route component inside a Routes component
-          and specify the path and element attributes as shown. */}
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/about-me" element={<AboutMe />} />
       </Routes>
     </div>
   );
