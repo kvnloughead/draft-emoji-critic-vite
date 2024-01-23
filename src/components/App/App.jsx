b694d421e341ae02aef0c4c537809520284e59d9
Kevin Loughead <kvnloughead@gmail.com>
2024-01-23 13:15:25 -0500

    adding-a-route-lesson-end

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,18 @@
+// App.js
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
 import Header from "../Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* Wrap a Route component inside a Routes component
+          and specify the path and element attributes as shown. */}
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
     </div>
   );
 }
