767ded7402cdf7871f2b7f9296f9707971acdb5b
Kevin Loughead <kvnloughead@gmail.com>
2024-01-23 16:32:38 -0500

    nesting-after-lesson

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,16 @@
 // App.js
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Header from "../Header/Header";
 import Dashboard from "../Dashboard/Dashboard";
 import Reviews from "../Reviews/Reviews";
-import AboutMe from "../AboutMe/AboutMe";
-import Header from "../Header/Header";
 import AboutUs from "../AboutUs/AboutUs";
+import AboutMe from "../AboutMe/AboutMe";
+
+// New imports
+import MyStory from "../AboutMe/MyStory";
+import Contact from "../AboutMe/Contact";
+import Hobbies from "../AboutMe/Hobbies";
 
 function App() {
   return (
@@ -14,7 +19,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/reviews" element={<Reviews />} />
-        <Route path="/about-me" element={<AboutMe />} />
+        {/* Add child routes to the /about-me route */}
+        <Route path="/about-me" element={<AboutMe />}>
+          <Route path="contact" element={<Contact />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="my-story" element={<MyStory />} />
+        </Route>
         <Route path="/about-us" element={<AboutUs />} />
       </Routes>
     </div>
