9ba5484fee5e2a6e341577442e723f62ae170920
Kevin Loughead <kvnloughead@gmail.com>
2024-01-24 14:12:20 -0500

    programmatic-navigation-after-lesson-no-task

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,12 +1,11 @@
 import "./Review.css";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Review({ reviews }) {
+  const navigate = useNavigate();
   const params = useParams();
   let id = params.reviewId;
-  // Decrement the id from the parameter so we access the correct items. This
-  // is necessary because the array indexes start with 0, whereas the IDs in //
-  // the API begin at 1.
   id = id - 1;
 
   return (
@@ -16,6 +15,10 @@ function Review({ reviews }) {
           <h3>{reviews[id].title}</h3>
           <p>{reviews[id].text}</p>
           <p className="review__rating">Final rating:{reviews[id].rating}/5</p>
+
+          <button onClick={() => navigate("/reviews")}>
+            Back to the review list
+          </button>
         </div>
       )}
     </div>
